dab9244e471d71782950ef41821d4f233c9fb997
storm <stooormix@gmail.com>
2022-01-22 01:35:37 +0330

    add toastify/simple Validator/react Helmet/react Router/react Router/react Router Dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom'
 import App from './App';
-
+import 'react-toastify/dist/ReactToastify.css';
 render(<App />,document.getElementById('root'));
 
 
