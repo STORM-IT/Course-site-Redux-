fbefb329c9360b948e1ddce4fe20b0bc9bf857c2
storm <stooormix@gmail.com>
2022-01-23 23:55:05 +0330

    add courses map in Course layout

--- src/Component/SiteLayout/Course.jsx
+++ src/Component/SiteLayout/Course.jsx
@@ -1,118 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Course = () => {
+const Course = ({courses}) => {
     return (
         <section className="terms-items">
             <header>
                 <h2> آخرین دوره های تاپ لرن </h2>
                 <Link to="/archive"> مشاهده همه دوره ها </Link>
             </header>
-            <div className="row">
+            {courses.map(course=>{
+                
+            <div key={course._id} className="row">
                 <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
                     <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/1.jpg" />
+                        <Link to={`/cource/${course._id}`} className="img-layer">
+                            <img src={`https://toplearnapi.ghorbany.dev/${course.imageUrl}`} />
                         </Link>
                         <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
+                            <Link to={`/cource/${course._id}`}> {course.title}</Link>
                         </h2>
-                        <span> رایگان </span>
+                        <span>{course.price}</span>
                         <i>1:52:32</i>
                     </article>
                 </div>
 
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/2.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> رایگان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/3.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> 150.000 تومان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/4.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> رایگان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/5.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> 15.000 تومان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/6.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> رایگان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/7.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> 20.000 تومان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
-
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 term-col">
-                    <article>
-                        <Link to="/course" className="img-layer">
-                            <img src="images/pic/8.jpg" />
-                        </Link>
-                        <h2>
-                            <a href=""> آموزش متریال دیاین در زامارین </a>
-                        </h2>
-                        <span> 75.000 تومان </span>
-                        <i>1:52:32</i>
-                    </article>
-                </div>
+                
             </div>
+            })}
         </section>
     );
 };
