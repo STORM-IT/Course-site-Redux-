737f3a307f63e8085d1c19cc86c26b7adc6b6181
storm <stooormix@gmail.com>
2022-01-25 23:03:31 +0330

    add jsonwebtoken and created decodeToken in Utils

--- src/App.js
+++ src/App.js
@@ -18,37 +18,40 @@ import Login from './Component/SiteLayout/Login'
 import Register from './Component/SiteLayout/Resister'
 import SingleCourse from './Component/SiteLayout/SingleCourse'
 import Course from './Component/SiteLayout/Course';
+import Navbar from './Component/SiteLayout/Navbar';
 
 import http from './Component/services/config.json'
 import { useSelector } from 'react-redux';
 import { paginate } from './Component/utils/PaginateIndexSlice';
-import Navbar from './Component/SiteLayout/Navbar';
 
 function App() {
   useEffect(() => {
-  require("./Component/JsFile/script")
+    require("./Component/JsFile/script")
+
   }, []);
-  
-  const courses=useSelector(state=>state.courses)
-  const IndexCourses=paginate(courses,1,8);
+  // const user =jwt.decode(localStorage.getItem("token"));
+  // console.log(user);
+
+  const courses = useSelector(state => state.courses)
+  const IndexCourses = paginate(courses, 1, 8);
   // console.log(courses);
   return (
     <Fragment>
       <Router>
         <Routes>
-          <Route path='/archive/*'  element={<Header />} />
+          <Route path='/archive/*' element={<Header />} />
           <Route path='/' exact element={<Head_Home />} />
         </Routes>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path='/about/Acount_Info' element={<Acount_Info />} />
           <Route path='/about/Acount_Edit' element={<Acount_Edit />} />
-          <Route path='/archive' element={<Archive courses={paginate(courses,1,12)} http={http.toplearnapi}/>} />
+          <Route path='/archive' element={<Archive courses={paginate(courses, 1, 12)} http={http.toplearnapi} />} />
           <Route path='/about/Lessen' element={<Lessen />} />
           <Route path='/about/Login' element={<Login />} />
           <Route path='/about/Register' element={<Register />} />
-          <Route path='/archive/singleCourse' element={<SingleCourse http={http.toplearnapi}/>} />
-          <Route path='/' element={<Course courses={IndexCourses}/>} />
+          <Route path='/archive/singleCourse' element={<SingleCourse http={http.toplearnapi} />} />
+          <Route path='/' element={<Course courses={IndexCourses} />} />
         </Routes>
         <Footer />
       </Router>
