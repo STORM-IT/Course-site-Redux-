8f4220b6bd6588d6e4737cf4dc3d3c5f80e75a1b
storm <stooormix@gmail.com>
2022-01-28 22:44:09 +0330

    create toastify option utils from import in userContext.jsx

---
+++ src/Component/utils/toastOption.js
@@ -0,0 +1,62 @@
+import { toast } from "react-toastify"
+
+export const toastify = (type , message) => {
+    switch (type) {
+
+        case "SUCCESS": return (
+
+            
+            toast.success(message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+        case "INFO": return (
+
+            toast.info(message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+        case "WARNING": return (
+
+            toast.warning(message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+        case "ERROR": return (
+
+            toast.error(message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+
+
+
+
+        default:
+            break;
+    }
+}
