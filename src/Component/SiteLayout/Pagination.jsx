9ef8eaeb5e7dc8e117f6cf4890249a136fa732db
storm <stooormix@gmail.com>
2022-02-04 21:38:48 +0330

    set before and after in pagination

--- src/Component/SiteLayout/Pagination.jsx
+++ src/Component/SiteLayout/Pagination.jsx
@@ -11,7 +11,7 @@ export default function Pagination({currentPage,totallCoursesLength,courseLength
     <nav aria-label="Page navigation">
     <ul className="pagination justify-content-center">
         <li className="page-item">
-            <a className="page-link" href="#" aria-label="Previous">
+            <a className="page-link" href="#" aria-label="Previous" onClick={currentPage!=1?()=>setPagination(currentPage-1):null}>
                 <span aria-hidden="true"><i className="zmdi zmdi-chevron-right"></i></span>
             </a>
         </li>
@@ -21,7 +21,7 @@ export default function Pagination({currentPage,totallCoursesLength,courseLength
         <li className={pagin===currentPage-1?"page-item active":"page-item"} key={pagin}><a className="page-link pointer" href="#" onClick={()=>setPagination(pagin+1)}>{pagin}</a></li>
     ))}
         <li className="page-item">
-            <a className="page-link" href="#" aria-label="Next">
+            <a className="page-link" href="#" aria-label="Next" onClick={currentPage!=IndexCourses?()=>setPagination(currentPage+1):null}>
                 <span aria-hidden="true"><i className="zmdi zmdi-chevron-left"></i></span>
             </a>
         </li>
