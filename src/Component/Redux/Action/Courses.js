f50b9680efe0dc37aa8afb337c744d06b4f777da
storm <stooormix@gmail.com>
2022-02-06 21:12:36 +0330

    create createNewCourse

--- src/Component/Redux/Action/Courses.js
+++ src/Component/Redux/Action/Courses.js
@@ -1,8 +1,21 @@
-import { getCourses } from './../../services/courseService'
-
+import { createCourse, getCourses } from './../../services/courseService'
+import {toastify} from '../../utils/toastOption'
 export const getAllCouses = () => {
     return async dispatch=>{
         const {data} =await getCourses();
         await dispatch({type:"INIT",payload:data.courses})
     }
 }
+
+
+export const createNewCourse = (course) => {
+    return async (dispatch, getState) => {
+        debugger
+        const { data, status } = await createCourse(course);
+        if (status === 201) toastify("SUCCESS","دوره با موفقیت ساخته شد");
+        await dispatch({
+            type: "ADD_COURSE",
+            payload: [...getState().courses, data.course],
+        });
+    };
+};
