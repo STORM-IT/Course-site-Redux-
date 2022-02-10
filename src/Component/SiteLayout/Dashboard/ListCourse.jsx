ce2c313676ce3194d8d9fe5d94752e42d9a8031b
storm <stooormix@gmail.com>
2022-02-10 23:31:23 +0330

    add dropdown and add sort state/create search box and set value / create sort switch in useEffect

--- src/Component/SiteLayout/Dashboard/ListCourse.jsx
+++ src/Component/SiteLayout/Dashboard/ListCourse.jsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React, { Fragment, useContext, useState } from 'react'
 import { useEffect } from 'react';
-import { Alert, Button, Form, Offcanvas, Table } from 'react-bootstrap'
+import { Alert, Button, Dropdown, Form, Offcanvas, Table } from 'react-bootstrap'
 import { useDispatch } from 'react-redux';
 import { context } from '../../ContextApi/context';
 import { removeCourseTable, searchCourse } from '../../Redux/Action/Courses';
@@ -12,36 +12,67 @@ import UpdateCourseCanvas from './updateCourseCanvas';
 // import {} from ''
 
 export default function List_Course({ courses }) {
-  const [currentPage, setCurrentPage] = useState(1);
-   const [Courses,setCourses]=useState(courses);
-   
-   const dispatch = useDispatch();
 
-   useEffect(() => {
-     setCourses(courses)
-   },[courses]);
+  const [Courses, setCourses] = useState(courses);
+  const [Sort,setSort]=useState();
 
-  const setPagination = (current) => {
-    setCurrentPage(current);
-  }
-  const {showCanvasCreate,setShowCanvasCreate,showCanvasUpdate,setShowCanvasUpdate,courseUpdate,setCourseUpdate} = useContext(context);
+  const dispatch = useDispatch();
 
-  const filteCourse=(character)=>{
+
+  useEffect(() => {
     
-    setCourses(courses.filter(course=>course.title.includes(character)))
+    debugger
+    setCourses(courses)
+    switch (Sort) {
+      case "HIGH":
+        Courses.sort(function(a, b){return   b.price - a.price})
+        break;
+        case "LOW":
+          Courses.sort(function(a, b){return a.price - b.price})
+          break;
+          
+          default:
+            break;
+          }
+          setCourses(courses)
+  }, [courses,Sort]);
+  // }, []);
+
+  
+  const { showCanvasCreate, setShowCanvasCreate, showCanvasUpdate, setShowCanvasUpdate, courseUpdate, setCourseUpdate, setPagination, currentPage } = useContext(context);
+  
+  const filteCourse = (character) => {
+    
+    setCourses(courses.filter(course => course.title.includes(character)))
   }
   const IndedCourses = paginate(Courses, currentPage, 9)
+  
   console.log("object");
-  const handle_show_update_course=(course)=>{
+  const handle_show_update_course = (course) => {
     setCourseUpdate(course)
     setShowCanvasUpdate(true)
   }
   return (
     <Fragment>
       <div className='alert alert-primary m-0 d-flex justify-content-between'>
-        <button className='btn btn-success btn-lg' onClick={()=> setShowCanvasCreate(true) }>+ Create course</button>
+
+
+        <button className='btn btn-success btn-lg' onClick={() => setShowCanvasCreate(true)}>+ Create course</button>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-button-dark-example1" size='lg' variant="secondary">
+            مرتب سازی قیمت از
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu variant="dark">
+            <Dropdown.Item onClick={()=>setSort("HIGH")}><h5>کم به زیاد</h5></Dropdown.Item>
+            <Dropdown.Item onClick={()=>setSort("LOW")}><h5>زیاد به کم</h5></Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={()=>setSort("")}><h5>بدون فیلتر</h5></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
         <div className='w-25'>
-        <Form.Control type="text" className='text-left' onChange={(e)=>filteCourse(e.target.value)} placeholder='Search'/>
+          <Form.Control type="text" className='text-left' onChange={(e) => filteCourse(e.target.value)} placeholder='Search' />
         </div>
       </div>
       <Table striped bordered hover variant='dark'>
@@ -60,20 +91,20 @@ export default function List_Course({ courses }) {
               <td>{course.title}</td>
               <td>{course.price == 0 ? "رایگان" : course.price}</td>
               <td >
-                <button onClick={()=>handle_show_update_course(course)} className='btn btn-warning btn-sm w-100 h-100'><h4>Edite</h4></button>
+                <button onClick={() => handle_show_update_course(course)} className='btn btn-warning btn-sm w-100 h-100'><h4>Edite</h4></button>
               </td>
-              <td><button onClick={()=>dispatch(removeCourseTable(course._id))} className='btn btn-danger btn-sm w-100 h-100'>Delete</button></td>
+              <td><button onClick={() => dispatch(removeCourseTable(course._id))} className='btn btn-danger btn-sm w-100 h-100'>Delete</button></td>
             </tr>
           ))}
         </tbody>
       </Table>
       <nav className="navbar fixed-bottom">
         <div className="container-fluid">
-         <Pagination currentPage={currentPage} totallCoursesLength={Courses.length} courseLength={9} setPagination={setPagination} />
+          <Pagination currentPage={currentPage} totallCoursesLength={Courses.length} courseLength={9} setPagination={setPagination} />
         </div>
       </nav>
-      {showCanvasCreate?<CourseNewCanvas/>:null}
-      {showCanvasUpdate?<UpdateCourseCanvas />:null}
+      {showCanvasCreate ? <CourseNewCanvas /> : null}
+      {showCanvasUpdate ? <UpdateCourseCanvas /> : null}
     </Fragment>
   )
 }
